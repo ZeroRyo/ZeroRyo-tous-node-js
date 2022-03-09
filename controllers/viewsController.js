02a89659db015c0d6a2ced947c87530e924cd35e
hà huy hùng <83862705+ZeroRyo@users.noreply.github.com>
2022-03-09 20:44:47 +0700

    Update bundle.js
    This reverts commit 06eff9887b442e30b77b94ec084053faeff92c97.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -42,14 +42,9 @@ exports.getLoginForm = (req, res) => {
         title: 'Log into your account'
     });
 };
-//getSignInForm
-exports.getSignInForm = (req, res) => {
-    res.status(200)
-    .set(
-        'Content-Security-Policy',
-        "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-      )
-    .render('basesignin', {
+//getSigninForm
+exports.getSigninForm = (req, res) => {
+    res.status(200).render('basesignin', {
         title: 'Sign in'
     });
 };
